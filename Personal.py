452f1dc97d49b12301c0547c48e10e765a5369e6
Nekocatso <3093490690@qq.com>
2024-04-18 09:39:08 +0800

    Refactor process_json function in Personal.py

--- Personal.py
+++ Personal.py
@@ -1,11 +1,13 @@
 import json
 import requests
-import time;
+import time
 import random
 
-def process_json(uid,end) :
+
+def process_json(uid, end):
     end_time = end
-    data = json.loads("""{
+    data = json.loads(
+        """{
     "uuid": "1",
     "experimentId": 473,
     "courseId": 2000077162,
@@ -497,30 +499,31 @@ def process_json(uid,end) :
             ]
         }
     ]
-}""")
-    data["uuid"]=uid
+}"""
+    )
+    data["uuid"] = uid
     data["endTime"] = end
-    data["score"]=100
+    data["score"] = 100
     all_time_use = 0
-    k=end_time
-    l=len(data["steps"])
-    for  i in range(l) :
-        step=data["steps"][l-1-i]
-        t=random.randint(4, 50)#未在17个模块中花费的时间
-        all_time_use+=t
-        k=k-t*1000
-        step["endTime"]=k
+    k = end_time
+    l = len(data["steps"])
+    for i in range(l):
+        step = data["steps"][l - 1 - i]
+        t = random.randint(4, 50)  # 未在17个模块中花费的时间
+        all_time_use += t
+        k = k - t * 1000
+        step["endTime"] = k
 
         t = step["expectTime"] + random.randint(4, 50)
         all_time_use += t
         step["timeUsed"] = t
         step["score"] = step["maxScore"]
-        k=k-t*1000
-        step["startTime"]=k
+        k = k - t * 1000
+        step["startTime"] = k
 
-    data["timeUsed"]=all_time_use
-    start_time = data["endTime"] -all_time_use*1000
-    data["startTime"]=start_time
+    data["timeUsed"] = all_time_use
+    start_time = data["endTime"] - all_time_use * 1000
+    data["startTime"] = start_time
 
     # i=0
     # for step in data["steps"]:
@@ -536,51 +539,37 @@ def process_json(uid,end) :
 
 
 
-def get_headers():
-    headers={
-        "Host": "virtualcourse.zhihuishu.com",
-        "Connection": "keep-alive",
-        "Content-Length": "23341",
-        "sec-ch-ua" : """" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100" """,
-        "sec-ch-ua-mobile": "?0",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
-        "sec-ch-ua-platform": "Windows",
-        "Content-Type":"application/x-www-form-urlencoded",
-        "Accept":"*/*",
-        "Origin" :"https://ar.zhihuishu.com",
-        "Sec-Fetch-Site":"same-site",
-        "Sec-Fetch-Mode":"cors",
-        "Sec-Fetch-Dest":"empty",
-        "Referer":"https//ar.zhihuishu.com/",
-        "Accept-Encoding":"gzip, deflate, br",
-        "Accept-Language":"zh-CN,zh;q=0.9 ",
-    }
-    return headers
+
 
 def first(uid):
-    url="https://virtualcourse.zhihuishu.com/report/queryUserName?uuid="+uid+"&courseId=2000077162&"
-    headers={
+    url = (
+        "https://virtualcourse.zhihuishu.com/report/queryUserName?uuid="
+        + uid
+        + "&courseId=2000077162&"
+    )
+    headers = {
         "Host": "virtualcourse.zhihuishu.com",
         "Connection": "keep-alive",
-        "sec-ch-ua" : """" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100" """,
+        "sec-ch-ua": """" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100" """,
         "sec-ch-ua-mobile": "?0",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
         "sec-ch-ua-platform": "Windows",
-        "Content-Type":"application/x-www-form-urlencoded",
-        "Accept":"*/*",
-        "Origin" :"https://ar.zhihuishu.com",
-        "Sec-Fetch-Site":"same-site",
-        "Sec-Fetch-Mode":"cors",
-        "Sec-Fetch-Dest":"empty",
-        "Referer":"https//ar.zhihuishu.com/",
-        "Accept-Encoding":"gzip, deflate, br",
-        "Accept-Language":"zh-CN,zh;q=0.9 ",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "*/*",
+        "Origin": "https://ar.zhihuishu.com",
+        "Sec-Fetch-Site": "same-site",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Dest": "empty",
+        "Referer": "https//ar.zhihuishu.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9 ",
     }
-    res=requests.get(url,headers=headers)
+    res = requests.get(url, headers=headers)
     print(res.text)
 
+
 def second():
-    url="https://studyservice.zhihuishu.com/api/stuExperiment/systemTime"
+    url = "https://studyservice.zhihuishu.com/api/stuExperiment/systemTime"
     headers = {
         "Host": "studyservice.zhihuishu.com",
         "Connection": "keep-alive",
@@ -597,27 +586,25 @@ def second():
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "zh-CN,zh;q=0.9 ",
     }
-    resp=requests.get(url,headers=headers)
+    resp = requests.get(url, headers=headers)
     data = resp.text
     print(data)
     data = json.loads(data)
     return data["data"]
 
 
-def third(uid,end):
+def third(uid, end):
     url = "https://virtualcourse.zhihuishu.com/report/saveReport"
-    data = process_json(uid,end)
-    data={"jsonStr":data,"ticket":None}
+    data = process_json(uid, end)
+    data = {"jsonStr": data, "ticket": None}
     headers = get_headers()
     response = requests.post(url, data=data, headers=headers)
     print(response.status_code)
     print(response.text)
 
 
-if __name__ == '__main__':
-    uid='XkML2qO1'#输入你的uuid运行即可
+if __name__ == "__main__":
+    uid = ""  # 输入你的uuid运行即可
     # 打开恰同学少年网页F12输入 getUUID()获得UUid替换上面再执行文件即可
-    end=second()
-    third(uid,end)
-
-
+    end = second()
+    third(uid, end)
